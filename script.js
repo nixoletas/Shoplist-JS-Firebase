c009afb34b4b6724d796b96789d9db4d76adf7b8
Nicholas_Miyasato <nicholetas@gmail.com>
2023-04-18 11:14:17 -0400

    first-upload

---
+++ script.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-47213-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+const deleteAllButton = document.getElementById("delete-all")
+
+deleteAllButton.addEventListener("click", function() {
+    
+    remove(shoppingListInDB);
+    
+    clearInputEl()
+})
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputEl()
+})
+
+onValue(shoppingListInDB, function(snapshot) {    
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        shoppingListEl.innerHTML = ""
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            
+            appendItemToShoppingListEl(currentItem)
+        } 
+    } else {
+        shoppingListEl.innerHTML = "Nenhum item na lista... ainda"
+    }
+})
+
+function clearInputEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingListEl.append(newEl)
+}
